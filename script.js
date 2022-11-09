d0ac27ff54ff05dc78db7f229b24209b1803bb92
ruandqm <ruandqm@gmail.com>
2022-11-09 20:24:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let soma = 2 + 2
+let subtracao = (-2) - (-4)
+let divisao = 80 / 3
+let resto = 80 % 3
+
+for (let increment = 0; increment < 5; increment++) {
+    soma++
+}
+
+for (let decrement = 0; decrement < 3; decrement++) {
+    soma--
+}
